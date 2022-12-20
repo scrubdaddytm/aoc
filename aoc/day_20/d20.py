40e09b2336372483fba264b5f0f12eb5768f4b38
scrubdaddytm <tucker@noia.me>
2022-12-20 14:22:57 -0600

    embarrassing day

--- aoc/day_20/d20.py
+++ aoc/day_20/d20.py
@@ -8,32 +8,36 @@ from copy import deepcopy
 class LinkedList:
     val: int = field(hash=True)
     original_idx: int = field(hash=True)
-    prev: "LinkedList" = field(hash=False, compare=False, default=None)
-    next: "LinkedList" = field(hash=False, compare=False, default=None)
-    def __repr__(self) -> str:
-        return f"{self.prev.val if self.prev else None} <-> {self.val} <-> {self.next.val if self.next else None}"
+    p: "LinkedList" = field(hash=False, compare=False, default=None)
+    n: "LinkedList" = field(hash=False, compare=False, default=None)
 
-    
+    def __repr__(self) -> str:
+        return f"{self.p.val if self.p else None} <-> {self.val} <-> {self.n.val if self.n else None}"
+
 
 def generate_linked_list(file: list[int]) -> tuple[LinkedList, dict[int, LinkedList]]:
-    start = prev = LinkedList(file[0], 0)
-    position = {file[0]: prev}
+    start = p = LinkedList(file[0], 0)
+    position = {(file[0], 0): p}
     for idx, val in enumerate(file[1:-1], 1):
-        entry = LinkedList(val, idx, prev=prev)
-        prev.next = entry
-        prev = entry
-        position[val] = entry
-    final = LinkedList(file[-1], len(file)-1)
-    final.prev = prev
-    final.next = start
-    position[final.val] = final
-    prev.next = final
-    start.prev = final
+        entry = LinkedList(val, idx, p=p)
+        p.n = entry
+        p = entry
+        position[(val, idx)] = entry
+    final = LinkedList(file[-1], len(file) - 1)
+    final.p = p
+    final.n = start
+    position[(final.val, final.original_idx)] = final
+    p.n = final
+    start.p = final
     return start, position
 
 
 def print_ll(start: LinkedList, count: int | None = None):
     print_str = ""
+    if count:
+        for _ in range(count // 2):
+            start = start.p
+        print_str = "..."
     seen = set()
     c = 0
     while not start in seen:
@@ -41,68 +45,82 @@ def print_ll(start: LinkedList, count: int | None = None):
             break
         seen.add(start)
         print_str += f"{start.val}, "
-        start = start.next
+        start = start.n
         c += 1
-    print(f"{print_str[:-2]}")
+    print(f"{print_str[:-2]}{'...' if count else ''}\n")
 
 
-def main() -> None:
-    # I give the wrong output :(
-    file = []
-    with file_input() as file_io:
-        while line := file_io.readline():
-            file.append(int(line))
-    print(f"{file}")
-    print(f"length of {len(file)} but count = {len(set(file))}")
-    linked_list, position = generate_linked_list(file)
+def get_distance(v, size):
+    size -= 1
+    if v == 0 or v % size == 0:
+        return 0
+    return v % size if v > 0 else size + (v % -size)
+
+
+def mix(file: list[int], position: dict[tuple[int, int], LinkedList]) -> int:
     size = len(file)
-    print_ll(position[0])
-    for val in file:
-        list_entry = position[val]
-        left = list_entry.val < 0
+    zero = None
+    for idx, val in enumerate(file):
+        list_entry = position[(val, idx)]
+        if val == 0:
+            zero = list_entry
 
-        modulo = -size if left else size
-        if list_entry.val % modulo == 0:
+        distance = get_distance(list_entry.val, size)
+
+        if distance == 0:
             print(f"{val} does not move")
             continue
 
+        print(f"{val} moves {distance} spaces forward")
+
         # Connect the homies
-        list_entry.prev.next = list_entry.next
-        list_entry.next.prev = list_entry.prev
+        list_entry.p.n = list_entry.n
+        list_entry.n.p = list_entry.p
 
-        im_next = list_entry
-        for _ in range(abs(list_entry.val % modulo)):
-            if left:
-                im_next = im_next.prev
-            else:
-                im_next = im_next.next
+        new_p = list_entry
+        for _ in range(distance):
+            new_p = new_p.n
 
-        if left:
-            # print(f"moving {val} between {im_next.prev.val} and {im_next.val}")
-            list_entry.next = im_next
-            list_entry.prev = im_next.prev
-            im_next.prev.next = list_entry
-            im_next.prev = list_entry
-        else:
-            # print(f"moving {val} between {im_next.val} and {im_next.next.val}")
-            list_entry.prev = im_next
-            list_entry.next = im_next.next
-            im_next.next.prev = list_entry
-            im_next.next = list_entry
-        print_ll(position[0], count=10)
+        new_n = new_p.n
+        new_n.p = list_entry
+        new_p.n = list_entry
+        list_entry.p = new_p
+        list_entry.n = new_n
 
-    start = zero = position[0]
-    print_ll(start)
+        print(f"moving {val} between {list_entry.p.val} and {list_entry.n.val}")
+        if size < 20:
+            print_ll(list_entry)
+
+    start = zero
+    print(f"{zero.p}{zero.n}")
     result = []
-    for distance in [1000, 2000, 3000]:
-        start = zero
-        tru_distance = distance % size
-        for _ in range(tru_distance):
-            start = start.next
-        result.append(start.val)
-    print(f"{result=} sums to {sum(result)}")
-    
+    for distance in range(0, 3001):
+        if distance > 0 and distance % 1000 == 0:
+            result.append(start.val)
+        start = start.n
+    return result
 
+
+def main() -> None:
+    file = []
+    decryption_key = 811589153
+    with file_input() as file_io:
+        while line := file_io.readline():
+            file.append(int(line))
+
+    print(f"length of {len(file)} but count = {len(set(file))}")
+    _, position = generate_linked_list(file)
+
+    print(f"part1: {sum(mix(file, position))}")
+
+    for idx in range(len(file)):
+        file[idx] = file[idx] * decryption_key
+
+    _, position = generate_linked_list(file)
+    result = 0
+    for _ in range(10):
+        result = mix(file, position)
+    print(f"part2: {sum(result)}")
 
 
 if __name__ == "__main__":
