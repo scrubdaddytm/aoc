6bad6526f23c65c9e9d6440bc7eeb97895bca8de
scrubdaddytm <tucker@noia.me>
2023-11-09 14:28:01 -0800

    2021 d5 p1

--- aoc/geometry.py
+++ aoc/geometry.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True, order=True)
 class Point:
     x: int
@@ -16,28 +17,45 @@ class Point:
 
 
 def up(p: Point, dist: int = 1) -> Point:
-    return Point(p.x, p.y+dist)
+    return Point(p.x, p.y + dist)
+
 
 def up_right(p: Point) -> Point:
-    return Point(p.x+1, p.y+1)
+    return Point(p.x + 1, p.y + 1)
+
 
 def up_left(p: Point) -> Point:
-    return Point(p.x-1, p.y+1)
+    return Point(p.x - 1, p.y + 1)
+
 
 def down(p: Point, dist: int = 1) -> Point:
-    return Point(p.x, p.y-dist)
+    return Point(p.x, p.y - dist)
+
 
 def down_left(p: Point) -> Point:
-    return Point(p.x-1, p.y-1)
+    return Point(p.x - 1, p.y - 1)
+
 
 def down_right(p: Point) -> Point:
-    return Point(p.x+1, p.y-1)
+    return Point(p.x + 1, p.y - 1)
+
 
 def left(p: Point, dist: int = 1) -> Point:
-    return Point(p.x-dist, p.y)
+    return Point(p.x - dist, p.y)
+
 
 def right(p: Point, dist: int = 1) -> Point:
-    return Point(p.x+dist, p.y)
+    return Point(p.x + dist, p.y)
+
 
 def distance(a: Point, b: Point) -> int:
     return abs(a.x - b.x) + abs(a.y - b.y)
+
+
+def get_line(a: Point, b: Point) -> list[Point]:
+    if a.x == b.x:
+        return [Point(a.x, new_y) for new_y in range(min(a.y, b.y), max(a.y, b.y) + 1)]
+    elif a.y == b.y:
+        return [Point(new_x, a.y) for new_x in range(min(a.x, b.x), max(a.x, b.x) + 1)]
+    else:
+        return []  # this might be needed someday
