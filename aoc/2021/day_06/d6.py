f7699965a6c118e1f90afe8cb5ee5698bd41b2d1
scrubdaddytm <tucker@noia.me>
2023-11-09 16:20:44 -0800

    2021 d6 p2 -- love a simpler rewrite

--- aoc/2021/day_06/d6.py
+++ aoc/2021/day_06/d6.py
@@ -1,28 +1,25 @@
 from aoc.cli import file_input
-from dataclasses import dataclass
-
-
-@dataclass
-class Lanternfish:
-    timer: int = 8
 
 
 def main() -> None:
     fish = []
     with file_input() as file:
         while line := file.readline():
-            fish = [Lanternfish(int(val)) for val in line.strip().split(",")]
+            fish = [int(val) for val in line.strip().split(",")]
 
-    for _ in range(80):
-        new_fish = []
-        for f in fish:
-            if f.timer == 0:
-                f.timer = 6
-                new_fish.append(Lanternfish())
-            else:
-                f.timer -= 1
-        fish.extend(new_fish)
-    print(f"part 1: {len(fish)}")
+    days = [[0, 0] for _ in range(7)]
+    for f in fish:
+        days[f][0] += 1
+    for day in range(256):
+        if day == 80:
+            print(f"part 1: {sum([day[0]+day[1] for day in days])}")
+        today = day % 7
+        new_fish_day = (day + 9) % 7
+
+        days[new_fish_day][1] += days[today][0]
+        days[today][0] += days[today][1]
+        days[today][1] = 0
+    print(f"part 2: {sum([day[0]+day[1] for day in days])}")
 
 
 if __name__ == "__main__":
