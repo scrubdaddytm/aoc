611b739fa8fee70bde7b22687d2de3c8755bd3cb
scrubdaddytm <tucker@noia.me>
2023-11-17 13:42:48 -0600

    2021 d13

--- aoc/print_tools.py
+++ aoc/print_tools.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from aoc.geometry import Point
+from collections.abc import Iterable
 
 
 class Color(str, Enum):
@@ -12,3 +14,31 @@ class Color(str, Enum):
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
     END = "\033[0m"
+
+
+def print_point_grid(
+    points: Iterable[Point],
+    max_x: int | None = None,
+    max_y: int | None = None,
+    min_x: int | None = None,
+    min_y: int | None = None,
+) -> None:
+    points = set(points)
+    if not max_x:
+        max_x = max(points, key=lambda p: p.x).x
+    if not max_y:
+        max_y = max(points, key=lambda p: p.y).y
+    if not min_x:
+        min_x = min(points, key=lambda p: p.x).x
+    if not min_y:
+        min_y = min(points, key=lambda p: p.y).y
+    print(f"{min_x=},{min_y=} -> {max_x=},{max_y=}")
+    print_str = ""
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            if Point(x, y) in points:
+                print_str += "#"
+            else:
+                print_str += "."
+        print_str += "\n"
+    print(print_str.strip())
