5372745d3c6518f52b84ac59c741a06ac485a304
scrubdaddytm <tucker@noia.me>
2023-11-20 16:28:36 -0600

    2021 d15 p2 -- p1 output is wrong now lol must be a shorter path going outside the original tile for my input

--- aoc/2021/day_15/d15.py
+++ aoc/2021/day_15/d15.py
@@ -36,9 +36,31 @@ def main() -> None:
             graph.append(list(map(int, list(line))))
 
     start = Point(0, 0)
-    end = Point(len(graph[0]) - 1, len(graph) - 1)
+    tile_x = len(graph[0])
+    tile_y = len(graph)
+    p1_end = Point(tile_x - 1, tile_y - 1)
+    end = Point((tile_x * 5) - 1, ((tile_y) * 5) - 1)
 
-    print(f"{start=}, {end=}")
+    print(f"{tile_x=}, {tile_y=}, {p1_end=}, {end=}")
+
+    point_graph = {}
+
+    for y in range(tile_y * 5):
+        for x in range(tile_x * 5):
+            point = Point(x, y)
+            dist = 9999999999
+            if x < tile_x and y < tile_y:
+                dist = graph[y][x]
+            else:
+                up = Point(x, y - tile_y)
+                left = Point(x - tile_x, y)
+                if in_bounds(up, tile_x * 5, tile_y * 5):
+                    dist = point_graph[up] + 1
+                else:
+                    dist = point_graph[left] + 1
+            if dist > 9:
+                dist = 1
+            point_graph[point] = dist
 
     distance = {}
     prev = {}
@@ -64,14 +86,15 @@ def main() -> None:
         for direction in CARDINAL_DIRECTIONS:
             next_vertex = direction(vertex)
             if in_bounds(next_vertex, max_x=end.x + 1, max_y=end.y + 1):
-                alt_dist = distance[vertex] + graph[next_vertex.y][next_vertex.x]
+                alt_dist = distance[vertex] + point_graph[next_vertex]
                 if alt_dist < distance[next_vertex]:
                     distance[next_vertex] = alt_dist
                     prev[next_vertex] = vertex
                     remove_from_pq(pq, next_vertex)
                     add_to_pq(pq, next_vertex, alt_dist)
 
-    print(f"part 1: {distance[end]}")
+    print(f"part 1: {distance[p1_end]}")
+    print(f"part 2: {distance[end]}")
 
 
 if __name__ == "__main__":
