7fe9b49de039abb1ebc9229339fb5adbb7b3a434
scrubdaddytm <tucker@noia.me>
2023-11-22 11:49:16 -0600

    2021 d16 p2 -- uggo but still correct

--- aoc/2021/day_16/d16.py
+++ aoc/2021/day_16/d16.py
@@ -27,7 +27,7 @@ def get_version_sum(packet: tuple) -> int:
         return version_sum
     version_sum += packet[0]
     if packet[1] != 4:
-        for sub_packet in packet[2]:
+        for sub_packet in packet[3]:
             version_sum += get_version_sum(sub_packet)
     return version_sum
 
@@ -36,7 +36,7 @@ def get_literal_value(binary: list[str]) -> int:
     bits = ""
     end_idx = None
     for idx in range(0, len(binary), 5):
-        bits += binary[idx+1:idx+5]
+        bits += binary[idx + 1 : idx + 5]
         if binary[idx] == "0":
             end_idx = idx + 5
             break
@@ -44,23 +44,18 @@ def get_literal_value(binary: list[str]) -> int:
 
 
 def decode(binary: str) -> str:
-    # print(f"DECODING -> {binary}")
     if not binary or int(binary, 2) == 0:
         return (None, None, None, None)
 
     version = int(binary[0:3], 2)
     packet_type = int(binary[3:6], 2)
     value = end_idx = None
+    sub_packets = []
 
-    if packet_type == 4:
-        value, end_idx = get_literal_value(binary[6:])
-        end_idx += 6
-    else:
+    if packet_type != 4:
         length_type_id = binary[6]
-        value = []
         if length_type_id == "0":
             sub_packet_bits = int(binary[7:22], 2)
-            # print(f"{sub_packet_bits=}")
             sub_packet_start = 22
             while sub_packet_start < 22 + sub_packet_bits:
                 sub_packet = decode(binary[sub_packet_start:])
@@ -68,11 +63,10 @@ def decode(binary: str) -> str:
                     sub_packet_start += sub_packet[-1]
                 else:
                     break
-                value.append(sub_packet)
+                sub_packets.append(sub_packet)
             end_idx = sub_packet_start
         else:
             sub_packet_count = int(binary[7:18], 2)
-            # print(f"{sub_packet_count=}")
             sub_packet_start = 18
             for packet_count in range(sub_packet_count):
                 sub_packet = decode(binary[sub_packet_start:])
@@ -80,10 +74,30 @@ def decode(binary: str) -> str:
                     sub_packet_start += sub_packet[-1]
                 else:
                     break
-                value.append(sub_packet)
+                sub_packets.append(sub_packet)
             end_idx = sub_packet_start
-    # print(version, packet_type, value, end_idx)
-    return (version, packet_type, value, end_idx)
+
+    if packet_type == 0:
+        value = sum([sp[2] for sp in sub_packets])
+    if packet_type == 1:
+        value = 1
+        for sp in sub_packets:
+            value *= sp[2]
+    if packet_type == 2:
+        value = min([sp[2] for sp in sub_packets])
+    if packet_type == 3:
+        value = max([sp[2] for sp in sub_packets])
+    elif packet_type == 4:
+        value, end_idx = get_literal_value(binary[6:])
+        end_idx += 6
+    if packet_type == 5:
+        value = 1 if sub_packets[0][2] > sub_packets[1][2] else 0
+    if packet_type == 6:
+        value = 1 if sub_packets[0][2] < sub_packets[1][2] else 0
+    if packet_type == 7:
+        value = 1 if sub_packets[0][2] == sub_packets[1][2] else 0
+
+    return (version, packet_type, value, sub_packets, end_idx)
 
 
 def main() -> None:
@@ -94,11 +108,11 @@ def main() -> None:
     print(f"{encoded_packets}")
 
     for encoded_packet in encoded_packets:
-        print(encoded_packet)
         binary = "".join(map(lambda c: HEX_TO_BINARY[c], list(encoded_packet)))
         packet = decode(binary)
-        print(packet)
-        print(f"Version Sum: {get_version_sum(packet)}")
+        print(
+            f"{encoded_packet}\n --> SUM: {get_version_sum(packet)}, VALUE: {packet[2]}"
+        )
     # print(packets)
 
 
