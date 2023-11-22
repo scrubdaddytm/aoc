ecf9beb831c2a2603e3b38391a80583c42bdadbc
scrubdaddytm <tucker@noia.me>
2023-11-22 13:54:15 -0600

    2021 d17 p1

--- aoc/print_tools.py
+++ aoc/print_tools.py
@@ -16,29 +16,45 @@ class Color(str, Enum):
     END = "\033[0m"
 
 
+def print_grid(
+    points: dict[Point, str],
+    max_x: int | None = None,
+    max_y: int | None = None,
+    min_x: int | None = None,
+    min_y: int | None = None,
+    invert_y: bool = False,
+):
+    if not max_x:
+        max_x = max(points.keys(), key=lambda p: p.x).x
+    if not max_y:
+        max_y = max(points.keys(), key=lambda p: p.y).y
+    if not min_x:
+        min_x = min(points.keys(), key=lambda p: p.x).x
+    if not min_y:
+        min_y = min(points.keys(), key=lambda p: p.y).y
+    print(f"{min_x=},{min_y=} -> {max_x=},{max_y=}")
+    print_str = []
+    for y in range(min_y, max_y + 1):
+        row = ""
+        for x in range(min_x, max_x + 1):
+            p = Point(x, y)
+            if p in points:
+                row += points[p]
+            else:
+                row += "."
+        print_str.append(row)
+    if invert_y:
+        print_str = reversed(print_str)
+    print("\n".join(print_str))
+
+
 def print_point_grid(
     points: Iterable[Point],
     max_x: int | None = None,
     max_y: int | None = None,
     min_x: int | None = None,
     min_y: int | None = None,
+    invert_y: bool = False,
 ) -> None:
-    points = set(points)
-    if not max_x:
-        max_x = max(points, key=lambda p: p.x).x
-    if not max_y:
-        max_y = max(points, key=lambda p: p.y).y
-    if not min_x:
-        min_x = min(points, key=lambda p: p.x).x
-    if not min_y:
-        min_y = min(points, key=lambda p: p.y).y
-    print(f"{min_x=},{min_y=} -> {max_x=},{max_y=}")
-    print_str = ""
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            if Point(x, y) in points:
-                print_str += "#"
-            else:
-                print_str += "."
-        print_str += "\n"
-    print(print_str.strip())
+    points = {p: "#" for p in points}
+    print_grid(points, max_x, max_y, min_x, min_y)
