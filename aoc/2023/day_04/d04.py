4fd3ad3d5d0b16716bbce7d22f8486a944c38c69
scrubdaddytm <tucker@noia.me>
2023-12-04 11:12:20 -0600

    couldnt help myself from getting rid of some redundant code

--- aoc/2023/day_04/d04.py
+++ aoc/2023/day_04/d04.py
@@ -7,22 +7,16 @@ def main() -> None:
         while line := file.readline().strip():
             lines.append(line.split(": "))
             lines[-1][1] = lines[-1][1].split(" | ")
-            winners = set(map(int, lines[-1][1][0].split()))
-            lines[-1][1][0] = winners
-            picked = set(map(int, lines[-1][1][1].split()))
-            lines[-1][1][1] = picked
+            lines[-1][1][0] = set(map(int, lines[-1][1][0].split()))
+            lines[-1][1][1] = set(map(int, lines[-1][1][1].split()))
 
     points = 0
     card_count = {k: 1 for k in range(len(lines))}
     for game_num, game in enumerate(lines):
-        for card_idx in range(card_count[game_num]):
-            winners = game[1][0]
-            picked = game[1][1]
-            winning_numbers = len(winners & picked)
-            if card_idx == 0:
-                points += int(2 ** (winning_numbers - 1))
-            for card_increase in range(game_num + 1, game_num + winning_numbers + 1):
-                card_count[card_increase] += 1
+        winning_numbers = len(game[1][0] & game[1][1])
+        points += int(2 ** (winning_numbers - 1))
+        for card_increase in range(game_num + 1, game_num + winning_numbers + 1):
+            card_count[card_increase] += card_count[game_num]
     print(f"part 1: {points=}")
     print(f"part 2: {sum(card_count.values())}")
 
