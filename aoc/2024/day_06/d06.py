a7edf73f6b4266472fb92fefcc63001ec8080057
tucker <tucker@noia.me>
2024-12-06 09:17:56 -0600

    slight cleanup

--- aoc/2024/day_06/d06.py
+++ aoc/2024/day_06/d06.py
@@ -1,12 +1,9 @@
 from aoc.cli import file_input
-from dataclasses import dataclass
 from aoc.geometry import up as down
 from aoc.geometry import down as up
 from aoc.geometry import left, right
 from aoc.geometry import Point
 from aoc.geometry import in_bounds
-from aoc.print_tools import print_grid
-from aoc.print_tools import print_point_grid
 
 
 ROTATE_DIR = {
@@ -22,7 +19,6 @@ def traverse(graph, guard, max_x, max_y) -> tuple[set, bool]:
     guard_dir = up
 
     while in_bounds(guard, max_x, max_y):
-        # print(f"{(guard, guard_dir.__name__)}")
         if (guard, guard_dir) in seen_with_dir:
             return seen_with_dir, True
         seen_with_dir.add((guard, guard_dir))
@@ -41,9 +37,6 @@ def main() -> None:
         while line := file.readline().strip():
             lines.append(line)
 
-    p1 = 0
-    p2 = 0
-
     graph = set()
     guard = None
 
@@ -54,13 +47,13 @@ def main() -> None:
             elif cell == "^":
                 guard = Point(j, i)
 
-    p1_seen, looped = traverse(graph, guard, len(lines), len(lines[0]))
+    p1_seen, _ = traverse(graph, guard, len(lines), len(lines[0]))
     p1_seen = {x[0] for x in p1_seen}
     print(f"Part 1: {len(p1_seen)}")
 
+    p2 = 0
     p1_seen.remove(guard)
     for i, p in enumerate(p1_seen):
-        print(i, p)
         graph.add(p)
         _, looped = traverse(graph, guard, len(lines), len(lines[0]))
         if looped:
