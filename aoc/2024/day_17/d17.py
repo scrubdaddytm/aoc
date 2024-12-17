9d371099e0dcfad056e601bd827a41f700addec5
tucker <tucker@noia.me>
2024-12-17 15:41:32 -0600

    cleanup

--- aoc/2024/day_17/d17.py
+++ aoc/2024/day_17/d17.py
@@ -1,7 +1,6 @@
 import re
 
 from aoc.cli import file_input
-from aoc.print_tools import Color
 
 """
 combo ops:
@@ -21,23 +20,6 @@ combo ops:
 7 - cdv : C = A / 2^ combo op
 """
 
-INSTS = {
-    0: "adv",
-    1: "bxl",
-    2: "bst",
-    3: "jnz",
-    4: "bxc",
-    5: Color.GREEN + "out" + Color.END,
-    6: "bdv",
-    7: "cdv",
-}
-DEBUG = False
-
-
-def p(out: str) -> None:
-    if DEBUG:
-        print(out, end=" -- ")
-
 
 def get_combo_op(op: int, registers: list[int]):
     if op <= 3:
@@ -52,42 +34,28 @@ def simulate(registers: list[int], program: list[int]) -> list[int]:
     output = []
     while instruction < len(program) - 1:
         op = program[instruction + 1]
-        p(
-            f"-- {INSTS[program[instruction]]} {program[instruction+1]}->{op=}: {registers}"
-        )
+        combo_op = get_combo_op(op, registers)
         inc = 2
         match program[instruction]:
             case 0:
-                op = get_combo_op(op, registers)
-                p(f"A = {registers[0]} >> {op}")
-                registers[0] = registers[0] >> op
+                registers[0] >>= combo_op
             case 1:
-                p(f"B = {registers[1]} ^ {op}")
-                registers[1] = registers[1] ^ op
+                registers[1] ^= op
             case 2:
-                op = get_combo_op(op, registers)
-                p(f"B = {op} % 8")
-                registers[1] = op % 8
+                registers[1] = combo_op % 8
             case 3:
                 if registers[0] != 0:
                     instruction = op
                     inc = 0
             case 4:
-                p(f"B = {registers[1]} ^ {registers[2]}")
-                registers[1] = registers[1] ^ registers[2]
+                registers[1] ^= registers[2]
             case 5:
-                op = get_combo_op(op, registers)
-                output.append(op % 8)
+                output.append(combo_op % 8)
             case 6:
-                op = get_combo_op(op, registers)
-                p(f"B = {registers[0]} >> {op}")
-                registers[1] = registers[0] >> op
+                registers[1] = registers[0] >> combo_op
             case 7:
-                op = get_combo_op(op, registers)
-                p(f"C = {registers[0]} >> {op}")
-                registers[2] = registers[0] >> op
+                registers[2] = registers[0] >> combo_op
         instruction += inc
-        p("\n")
     return output
 
 
@@ -105,22 +73,14 @@ def backtrack(
     p_idx: int,
 ) -> int | None:
     if p_idx < 0:
-        a = list_to_int(a_trips)
-        result = simulate([a, 0, 0], program)
-        if result == program:
-            print(f" {result=}")
-            return a
-        return None
+        return list_to_int(a_trips)
 
     for potential_a_oct in range(8):
         maybe_a = a_trips + [potential_a_oct]
         a = list_to_int(maybe_a)
 
-        simulated_result = simulate([a, 0, 0], program)
-
-        if simulated_result == program[p_idx:]:
-            result = backtrack(program, maybe_a, p_idx - 1)
-            if result:
+        if simulate([a, 0, 0], program) == program[p_idx:]:
+            if result := backtrack(program, maybe_a, p_idx - 1):
                 return result
 
     return None
@@ -137,7 +97,6 @@ def main() -> None:
     p1 = simulate(registers, program)
 
     p2 = backtrack(program, [], len(program) - 1)
-    print(f"{program=}")
 
     print(f"Part 1: {','.join(map(str, p1))}")
     print(f"Part 2: {p2}")
