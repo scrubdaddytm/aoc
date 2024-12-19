cfc75add8fc9e94163c38321480bc8a86c7f6827
tucker <tucker@noia.me>
2024-12-19 07:10:28 -0600

    such a small solution for such a late day

---
+++ aoc/2024/day_19/d19.py
@@ -0,0 +1,36 @@
+from functools import cache
+
+from aoc.cli import file_input
+
+
+@cache
+def find_combo(towels: tuple[str], target: str) -> bool:
+    if not target:
+        return 1
+
+    possibilities = 0
+    for towel in towels:
+        if towel == target[: len(towel)]:
+            possibilities += find_combo(towels, target[len(towel):])
+
+    return possibilities
+
+
+def main() -> None:
+    towels = []
+    p1 = 0
+    p2 = 0
+    with file_input() as file:
+        while line := file.readline().strip():
+            towels = line.split(", ")
+        while line := file.readline().strip():
+            possibilities = find_combo(tuple(towels), line)
+            p1 += 1 if possibilities else 0
+            p2 += possibilities
+
+    print(f"Part 1: {p1}")
+    print(f"Part 2: {p2}")
+
+
+if __name__ == "__main__":
+    main()
