ac2a194df0302b7c1754a34ab9f87cf9b43bac93
tucker <tucker@noia.me>
2025-01-07 12:43:05 -0600

    dont like how p2 was worded...

--- aoc/y_2019/day_04/d04.py
+++ aoc/y_2019/day_04/d04.py
@@ -1,16 +1,45 @@
+from collections import Counter
+from itertools import pairwise
+
 from aoc.cli import file_input
-from dataclasses import dataclass
+
+
+def meets_password_criteria_p2(password: int) -> bool:
+    pass_str = str(password)
+    for a, b in pairwise(str(password)):
+        if b < a:
+            return False
+
+    char_count = Counter(pass_str)
+    return 2 in set(char_count.values())
+
+
+def meets_password_criteria_p1(password: int) -> bool:
+    found_double = False
+    for a, b in pairwise(str(password)):
+        if b < a:
+            return False
+        if a == b:
+            found_double = True
+
+    return found_double
 
 
 def main() -> None:
-    lines = []
+    lower_bound = upper_bound = 0
     with file_input() as file:
-        while line := file.readline():
-            lines.append(line)
-    print(f"{lines}")
+        line = file.readline().strip().split("-")
+        lower_bound = int(line[0])
+        upper_bound = int(line[1])
 
     p1 = 0
     p2 = 0
+
+    for password in range(lower_bound, upper_bound + 1):
+        if meets_password_criteria_p1(password):
+            p1 += 1
+        if meets_password_criteria_p2(password):
+            p2 += 1
 
     print(f"Part 1: {p1}")
     print(f"Part 2: {p2}")
