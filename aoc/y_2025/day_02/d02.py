ff86362243331545119f9cd73fdcb778f9de803b
tucker <tucker@noia.me>
2025-12-02 07:25:50 -0600

    minor janitorial work

--- aoc/y_2025/day_02/d02.py
+++ aoc/y_2025/day_02/d02.py
@@ -2,29 +2,13 @@ from itertools import batched
 
 from aoc.cli import file_input
 
-"""
-11-22 still has two invalid IDs, 11 and 22.
-95-115 now has two invalid IDs, 99 and 111.
-998-1012 now has two invalid IDs, 999 and 1010.
-1188511880-1188511890 still has one invalid ID, 1188511885.
-222220-222224 still has one invalid ID, 222222.
-1698522-1698528 still contains no invalid IDs.
-446443-446449 still has one invalid ID, 446446.
-38593856-38593862 still has one invalid ID, 38593859.
-565653-565659 now has one invalid ID, 565656.
-824824821-824824827 now has one invalid ID, 824824824.
-2121212118-2121212124 now has one invalid ID, 2121212121.
-"""
-
 
 def find_invalid(i: int) -> bool:
     i_str = str(i)
-    str_len = len(i_str)
-    for repeat in range(1, (str_len // 2) + 1):
-        if str_len % repeat != 0:
+    for repeat in range(1, (len(i_str) // 2) + 1):
+        if len(i_str) % repeat != 0:
             continue
         seen = set(["".join(batch) for batch in batched(i_str, repeat)])
-        # print(f"{i}: {repeat} -> {seen}{'<-------------' if len(seen) == 1 else ''}")
         if len(seen) == 1:
             return True
 
@@ -50,9 +34,8 @@ def main() -> None:
             i_str = str(i)
             if i_str[len(i_str) // 2:] == i_str[: len(i_str) // 2]:
                 p1 += i
-
-            bad = find_invalid(i)
-            if bad:
+                p2 += i
+            elif find_invalid(i):
                 p2 += i
 
     print(f"Part 1: {p1}")
